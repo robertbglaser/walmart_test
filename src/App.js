c03c23d052d957382882d92698023ca328771e6a
Robert Glaser <robertbglaser@gmail.com>
2019-04-05 18:14:47 -0700

    the two give images now render

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 import InputArea from './InputArea';
+import ImageCard from './ImageCard';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
       <InputArea/>
-        
+      <ImageCard 
+      imageUrl={"https://res.cloudinary.com/twenty20/private_images/t_watermark-criss-cross-10/v1450248078000/photosp/e41c1f0f-2ed5-4f78-b92b-9900340b5d1a/stock-photo-river-travel-adventure-walking-foot-feet-boots-hike-going-e41c1f0f-2ed5-4f78-b92b-9900340b5d1a.jpg"}
+      Title='hiking Boots'
+       /> 
+       <ImageCard 
+      imageUrl={"https://thumbs.dreamstime.com/z/zip-line-26475548.jpg"}
+      Title='Zip Line'
+       /> 
       </div>
     );
   }
